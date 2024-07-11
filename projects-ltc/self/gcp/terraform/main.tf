34e16a7a5bda1745cc0ee3d181e3b9e694eda35d
navathagoud <navatha.anguru@gmail.com>
2024-07-11 22:36:44 +0530

    created terraform scripts for gcp vm

---
+++ projects-ltc/self/gcp/terraform/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+    project = "My-first-project"
+    region  = "us-west1"
+    zone    = "us-west1-a"
+}
+
+resource "google_compute_instance" "main" {
+    name         = "test-vm-tf"
+    machine_type = "e2-micro"
+
+    network_interface {
+        network = "test-vpc"
+    }
+
+    boot_disk {
+        initialize_params {
+            image = "Debian GNU/Linux 12 (bookworm)"
+        }
+    }
+    
+}
