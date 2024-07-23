aa25af2fc14656fb22e6d67f87368cc2ccc2ecbb
navathagoud <navatha.anguru@gmail.com>
2024-07-23 22:06:08 +0530

    committed the untracked files

--- projects-ltc/self/aws/custom_modules/dev/backend.tf
+++ projects-ltc/self/aws/custom_modules/dev/backend.tf
@@ -1,6 +1,11 @@
 terraform {
   backend "s3" {
     bucket = "tfstate-bkp-s3"
-    region = "eu-north-1"
-  }
+    region = "eu-north-1"}
 }
+
+
+
+
+
+
--- projects-ltc/self/gcp/terraform/main.tf
+++ projects-ltc/self/gcp/terraform/main.tf
@@ -1,12 +1,15 @@
+terraform {
+  backend "gcs" {
+    bucket = "tfstate-bkp-gcs"
+    prefix = "terraform/tf/tf-state" 
+  }
+}
+
 provider "google" {
-##    credentials = "../sa-creds.json"
     project = "advance-totem-428319-n5"
     region  = "us-west1"
     zone    = "us-west1-a"
-
-    
 }
-
 
 resource "google_compute_instance" "main" {
     name         = "test-vm-tf"
